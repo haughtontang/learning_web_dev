0dad9a32a45fb340b5b79f3bfa2bd2ea62f31949
haughtontang <donhaughton@hotmail.co.uk>
2022-03-22 20:13:18 +0000

    added practice by changing background color and text color on condition

--- basic_form_with_js/form_js.js
+++ basic_form_with_js/form_js.js
@@ -5,8 +5,9 @@ const nameInput = document.querySelector('#name')
 const emailInput = document.querySelector('#email')
 const msg = document.querySelector('.msg')
 const userList = document.querySelector('#users')
-const varArray = [myForm, nameInput, emailInput, msg, userList]
-
+const body = document.body
+const header = document.querySelector('#form_header')
+const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 // Need to define an event listener to tell when a user has clicked a button
 // Args - the input to listen for and what to do when that action is performed
 myForm.addEventListener('submit', onSubmit);
@@ -29,6 +30,33 @@ function onSubmit(e){
 
         // Appends those values to the ul created in the html file
         userList.appendChild(li);
+
+        // Changes the colors for 3 seconds based on the first letter of the name
+        const firstChar = nameInput.value.toLowerCase().charAt(0);
+        const indexOfFirstChar = alphabet.indexOf(firstChar);
+        let styleClass = '';
+        if(indexOfFirstChar >= 0 & indexOfFirstChar < 9){
+            body.classList.add('blue');
+            header.classList.add('blue');
+            styleClass = 'blue';
+        }
+        else if(indexOfFirstChar >= 9 & indexOfFirstChar < 18){
+            body.classList.add('green');
+            header.classList.add('green');
+            styleClass = 'green';
+        } else {
+            body.classList.add('purple');
+            header.classList.add('purple');
+            styleClass = 'purple';
+        }
+
+        // Changes the style for 2.5 seconds
+        setTimeout(function () {
+            body.classList.remove(styleClass);
+            header.classList.remove(styleClass);
+            }, 2500);
+        body.classList.add('body');
+        header.classList.add('form_header');
 
         // Once appended, clear the input fields
         nameInput.value = '';
